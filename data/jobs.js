cacc7fc75bed76e5dbf3100c18c970c78c59593c
A Statistical Analysis of Fast food consumption Behaviour of Students in Tirupati <chglory8888@gmail.com>
2026-08-11 20:28:59 +0530

    Create jobs.js

---
+++ data/jobs.js
@@ -0,0 +1,13 @@
+export const jobs = [
+  {
+    id: 1,
+    title: "SSC CGL 2026 Recruitment",
+    category: "SSC",
+    posts: "12256",
+    qualification: "Graduation",
+    lastDate: "2026-09-15",
+    applyLink: "https://ssc.gov.in",
+    salary: "₹50,000 – ₹1,51,100",
+    status: "Open"
+  }
+];
