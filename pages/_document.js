a5efc0a811f4ae0dce5eb21d15953308b461ac3c
A Statistical Analysis of Fast food consumption Behaviour of Students in Tirupati <chglory8888@gmail.com>
2026-08-11 23:48:28 +0530

    Create _document.js

---
+++ pages/_document.js
@@ -0,0 +1,19 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="dIBVdX3k2APZaWxk8hlGB7ykx_gDwHzGQXNtoswegjk"
+        />
+      </Head>
+
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
