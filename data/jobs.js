421e98e5de588b158db1cc8806b84d5867d56f27
A Statistical Analysis of Fast food consumption Behaviour of Students in Tirupati <chglory8888@gmail.com>
2026-08-11 22:32:57 +0530

    Update jobs.js

--- data/jobs.js
+++ data/jobs.js
@@ -1,3 +1,4 @@
+```javascript
 export const jobs = [
   {
     id: 1,
@@ -5,9 +6,10 @@ export const jobs = [
     category: "SSC",
     posts: "12256",
     qualification: "Graduation",
-    lastDate: "2026-09-15"",
+    lastDate: "2026-09-15",
     applyLink: "https://ssc.gov.in",
     salary: "₹50,000 – ₹1,51,100",
     status: "Open"
   }
 ];
+```
