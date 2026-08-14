e72273c9439136a34570ca2b2085f4d59b5b3ca0
A Statistical Analysis of Fast food consumption Behaviour of Students in Tirupati <chglory8888@gmail.com>
2026-08-14 12:38:51 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,34 +1,8 @@
 import "../styles/globals.css";
-import Link from "next/link";
 
-export default function App({ Component, pageProps }) {
-  return (
-    <>
-      <nav className="navbar">
-        <div className="nav-container">
-
-          <Link href="/" className="logo">
-            💼 Govt Jobs 2026
-          </Link>
-
-          <div className="nav-links">
-            <Link href="/">
-              Home
-            </Link>
-
-            <Link href="/about">
-              About
-            </Link>
-
-            <Link href="/contact">
-              Contact
-            </Link>
-          </div>
-
-        </div>
-      </nav>
-
-      <Component {...pageProps} />
-    </>
-  );
+export default function App({
+  Component,
+  pageProps,
+}) {
+  return <Component {...pageProps} />;
 }
